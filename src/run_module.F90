88108cc2028d3164c359f6142d34070eccea23da
hoonhout <hoonhout@0ff102cf-ee28-4064-adf9-66d38ca607b7>
2014-05-21 11:20:35 +0000

    Added progress indicator and bedmodule
    git-svn-id: https://svn.oss.deltares.nl/repos/openearthtools/trunk/fortran/AeoLiS@10717 0ff102cf-ee28-4064-adf9-66d38ca607b7

--- src/run_module.F90
+++ src/run_module.F90
@@ -5,6 +5,8 @@ module run_module
   use bed_module
   use supply_module
 
+  implicit none
+
 contains
 
   subroutine run_model(par)
@@ -12,6 +14,7 @@ contains
     type(parameters), intent(inout) :: par
     integer*4 :: i, ti, si, n, l
     real*8 :: t, dt, dx, Ta
+    real*8 :: tstart
     real*8, dimension(:), allocatable :: x, z, u_th
     real*8, dimension(:), allocatable :: u, t_supply
     real*8, dimension(:,:), allocatable :: supply
@@ -27,6 +30,7 @@ contains
 
     ! bed
     call generate_bed(par, x, z)
+    call generate_bedcomposition(par, x, z)
     open(unit=fid, file="bed", action="write", status="replace", form="unformatted")
     write(fid) x
     write(fid) z
@@ -66,23 +70,27 @@ contains
 
     write(*,*) 'Model run started...'
 
+    ! update threshold
+    u_th = par%u_th
+    call compute_threshold_bedslope(par, x, z, u_th)
+
     ! model
     open(unit=fid+1, file="Ct", action="write", status="replace", form="unformatted")
     open(unit=fid+2, file="Cu", action="write", status="replace", form="unformatted")
     open(unit=fid+3, file="Ca", action="write", status="replace", form="unformatted")
-    open(unit=fid+4, file="u_th", action="write", status="replace", form="unformatted")
+
+    si = 1
+    tstart = get_time()
     do ti=1,par%nt
 
-       ! update threshold
-       u_th = par%u_th
-       call compute_threshold_bedslope(par, x, z, u_th)
-
        ! update supply
-       si = binary_search(t_supply, t)
+       if (t .gt. t_supply(si+1)) then
+          si = si + 1
+       end if
 
        ! log progress
        if ( mod(dble(ti), par%nt/10.d0) == 0 ) then
-          write(*,'(f5.1,a)') 100.d0*ti/par%nt, '%'
+          call write_progress(ti, par%nt, tstart)
        end if
 
        do i=2,par%nx+1
@@ -109,7 +117,6 @@ contains
           write(fid+1) Ct
           write(fid+2) Cu
           write(fid+3) Ca
-          write(fid+4) u_th
        end if
        
        t = t + par%dt
@@ -122,5 +129,63 @@ contains
     write(*,*) 'Done.'
 
   end subroutine run_model
+  
+  subroutine write_progress(ti, nt, tstart)
+
+    integer*4, intent(in) :: ti, nt
+    real*8, intent(in) :: tstart
+    real*8 :: p, dt1, dt2, dt3
     
+    p = dble(ti) / nt
+    dt1 = get_time() - tstart
+    dt2 = dt1 / p
+    dt3 = dt2 * (1 - p)
+
+    write(*,'(f5.1,a2,a8,a3,a8,a3,a8)') 100.d0*p, '% ', format_time(dt1), ' / ', format_time(dt2), ' / ', format_time(dt3)
+
+  end subroutine write_progress
+
+  function get_time() result (tm)
+
+    integer*4 :: count,count_rate,count_max
+    real*8 :: tm
+
+    call system_clock (count,count_rate,count_max)
+    tm = dble(count)/count_rate
+
+  end function get_time
+
+  function format_time(tm) result (str)
+
+    real*8, intent(in) :: tm
+    integer*4 :: h, m, s
+    character(2) :: h_s, m_s, s_s
+    character(8) :: str
+    
+    h = int(tm / 3600)
+    m = int((tm - h * 3600) / 60)
+    s = nint(tm - h * 3600 - m * 60)
+
+    if (h < 10) then
+       write(h_s, '("0",i0)') h
+    else
+       write(h_s, '(i0)') h
+    end if
+
+    if (m < 10) then
+       write(m_s, '("0",i0)') m
+    else
+       write(m_s, '(i0)') m
+    end if
+
+    if (s < 10) then
+       write(s_s, '("0",i0)') s
+    else
+       write(s_s, '(i0)') s
+    end if
+
+    str = h_s // ':' // m_s // ':' // s_s
+
+  end function format_time
+
 end module run_module
